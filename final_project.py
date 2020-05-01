a79d18c7b65fb515016fbacd3805f3d3290195ed
Ryan Bell <rtbell@wisc.edu>
2020-05-01 03:11:38 -0500

    clean

--- final_project.py
+++ final_project.py
@@ -3,10 +3,6 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as seabornInstance
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn import metrics
 import math
 import random
 import copy
